605c17de45969b8721a848020a8f7327203a01ff
Ryan Allred <Ryan@Allred.xyz>
2018-02-27 09:47:39 -0700

    Add stubs for login & map

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages'
+import LoginPage from './pages/login'
+import MapPage from './pages/map'
 import DealPage from './pages/deal'
 import OrderPage from './pages/order'
 
 export default () => (
-  <HomePage>
-    <Route path="/:selectedId">
-      <DealPage>
-        <Route path="/:selectedId/order" component={OrderPage} />
-      </DealPage>
+  <Switch>
+    <Route path="/login">
+      <LoginPage />
     </Route>
-  </HomePage>
+    <Route path="/map">
+      <MapPage />
+    </Route>
+    <Route path="/">
+      <HomePage>
+        <Route path="/:selectedId">
+          <DealPage>
+            <Route path="/:selectedId/order" component={OrderPage} />
+          </DealPage>
+        </Route>
+      </HomePage>
+    </Route>
+  </Switch>
 )
