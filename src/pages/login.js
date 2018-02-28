21a02ab21fba302ff20b40791bfdb07f48042513
Ryan Walker <RyanPrestonWalker@gmail.com>
2018-02-28 09:34:57 -0700

    Fixed img styling

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,13 +3,18 @@ import Header from '../components/header'
 import Main from '../components/main'
 import GoogleLogin from '../assets/images/google-login.png'
 import FacebookLogin from '../assets/images/facebook-login.png'
-import { Image } from 'react-feather';
+import { Image, Facebook } from 'react-feather';
 
 export default () => (
   <div>
     <Header nav>Login</Header>
     <Main>
-      <img src={GoogleLogin}></img>
+      <img src={GoogleLogin}
+        style={{width: '100%'}}>
+      </img>
+      <img src={FacebookLogin}
+        style={{width: '100%'}}>
+      </img>
     </Main>
   </div>
 )
