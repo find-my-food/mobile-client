8368a5d0285fc07717f55fd672ce939e0cbc5f91
Ryan Allred <Ryan@Allred.xyz>
2018-02-28 10:27:20 -0700

    Clean up eslint errors

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,18 +3,17 @@ import Header from '../components/header'
 import Main from '../components/main'
 import GoogleLogin from '../assets/images/google-login.png'
 import FacebookLogin from '../assets/images/facebook-login.png'
-import { Image, Facebook } from 'react-feather';
 
 export default () => (
   <div>
     <Header nav>Login</Header>
     <Main>
-      <img src={GoogleLogin}
-        style={{width: '100%', paddingTop: '50%'}}>
-      </img>
-      <img src={FacebookLogin}
-        style={{width: '100%'}}>
-      </img>
+      <img
+        alt=""
+        src={GoogleLogin}
+        style={{ width: '100%', paddingTop: '50%' }}
+      />
+      <img alt="" src={FacebookLogin} style={{ width: '100%' }} />
     </Main>
   </div>
 )
