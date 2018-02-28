7a4a5cf801d23887832363ab58a67cfd7d46317a
Ryan Walker <RyanPrestonWalker@gmail.com>
2018-02-28 09:28:04 -0700

    Added assets folder, images, and deleted var.js

--- src/components/vars.js
+++
@@ -1,1 +0,0 @@
-export const BASE_COLOR = '#0044b3'
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../components/header'
 import Main from '../components/main'
+import GoogleLogin from '../assets/images/google-login.png'
+import FacebookLogin from '../assets/images/facebook-login.png'
+import { Image } from 'react-feather';
 
 export default () => (
   <div>
     <Header nav>Login</Header>
-    <Main>Login Here</Main>
+    <Main>
+      <img src={GoogleLogin}></img>
+    </Main>
   </div>
 )
