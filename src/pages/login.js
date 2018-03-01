45f7659e655234ac9f220ec3a396c127ba20569a
Ryan Walker <RyanPrestonWalker@gmail.com>
2018-03-01 09:25:00 -0700

    Added logo

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,11 +3,14 @@ import Header from '../components/header'
 import Main from '../components/main'
 import GoogleLogin from '../assets/images/google-login.png'
 import FacebookLogin from '../assets/images/facebook-login.png'
+import Logo from '../assets/images/logo.png'
 
 export default () => (
   <div>
     <Header nav>Login</Header>
     <Main>
+      <img alt='logo' src={Logo} style={{ width: '100%', paddingTop: '25%' }}
+      />
       <img
         alt=""
         src={GoogleLogin}
