1bb6d01845d3e1742759916c652ba6734ad5d7fd
Ryan Walker <RyanPrestonWalker@gmail.com>
2018-03-02 09:57:38 -0700

    Souped up rewards page

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -1,23 +1,45 @@
 import React from 'react'
+import { compose, withProps } from 'recompose'
 import Header from '../components/header'
 import Main from '../components/main'
-import Check from '../components/check'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
 
-export default () => (
+const Rewards = styled.div`
+background: #fff;
+height: 50px;
+line-height: 50px;
+font-size: 18px;
+text-align: center;
+color:black;
+text-shadow: 0px 0.5px 0px rgba(0, 0, 0, 0.3);
+padding: 0 20px;
+box-shadow: 0px -1px 0px 0px #0000006b inset;
+`
+
+const enhance = compose(
+  connect(state => ({ places: state.data.places })),
+  withProps(({ places }) => ({
+    places: Object.keys(places)
+      .map(k => places[k])
+  }))
+)
+
+const Component = ({ places }) => (
   <div>
-    <Header nav>Rewards</Header>
+    <Header nav>Find My Food</Header>
     <Main>
-      <p
-        style={{
-          fontSize: '2em',
-          width: '100%',
-          top: '50%',
-          display: 'flex',
-          justifyContent: 'center'
-        }}>
-        Click for a reward!
-      </p>
+        {places.map((place, index) => (
+          <Rewards key={index} >
+            <div style={{ textAlign: 'left' }}>
+              {place.name}
+            </div>
+          </Rewards>
+        ))}
     </Main>
-    <Check />
   </div>
 )
+
+const RewardsComponent = enhance(Component)
+
+export default RewardsComponent
